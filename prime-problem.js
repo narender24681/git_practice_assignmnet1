51f674f03faab3184b0593c1071d1c140bd3f010
Narender <narender24681@gmail.com>
2022-11-02 11:39:37 +0530

    Created prime-problem.js

---
+++ prime-problem.js
@@ -0,0 +1,17 @@
+// Print Prime Numbers from 1 to given limit
+
+var limit = 100;
+
+for (var i = 1; i <= limit; i++) {
+  var divisibleCount = 0;
+
+  for (var j = 1; j <= limit; j++) {
+    if (i % j == 0) {
+      divisibleCount++;
+    }
+  }
+
+  if (divisibleCount == 3) {
+    console.log(i);
+  }
+}
